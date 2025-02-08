652c5ce940285ed7b56cb694268ec5b339fa715f
kuter555 <ch.wahlen01@gmail.com>
2025-02-08 19:04:53 +0000

    Final fixes

--- train.py
+++ train.py
@@ -12,19 +12,45 @@ import os
 from PIL import Image
 import numpy as np
 
-from vqvae import VAE
-import matplotlib.pyplot as plt
+from models.vqvae import VAE
+from utils import print_progress_bar
 
-import sys
-
-
-def print_progress_bar(epoch, iteration, total, length=50):
-    progress = int(length * iteration / total)
-    bar = f"\033[31m Epoch {epoch}:\033[97m [{'=' * progress}{' ' * (length - progress)}] {int(100 * iteration / total)}%"
-    sys.stdout.write(f"\r{bar}")
-    sys.stdout.flush()
+class CustomImageFolder(Dataset):
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+        self.image_files = []
+        
+        self.transform =  transforms.Compose([
+            transforms.Resize((128, 128)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,))
+        ])
+        
+        # Supported image extensions
+        self.valid_extensions = {'.jpg', '.jpeg', '.png', '.bmp', '.gif'}
+        
+        # Collect all valid image files
+        for root, dirs, files in os.walk(root_dir):
+            for file in files:
+                if any(file.lower().endswith(ext) for ext in self.valid_extensions):
+                    self.image_files.append(os.path.join(root, file))
+        
+    def __len__(self):
+        return len(self.image_files)
     
-    
+    def __getitem__(self, idx):
+        image_path = self.image_files[idx]
+        image = Image.open(image_path).convert("RGB")  # Open image and ensure it's RGB
+        
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, 0  # We don't need labels for VAE (return dummy label 0)
+
+
+
+
+
 
 
 def train_vae(load=False):
