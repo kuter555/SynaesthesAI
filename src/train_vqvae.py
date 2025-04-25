9013df62bc66b475ad8f04b0a7a7257cc37bc6a8
kuter555 <ch.wahlen01@gmail.com>
2025-04-26 00:52:00 +0100

    Fix pixel misalignment issue

--- src/train_vqvae.py
+++ src/train_vqvae.py
@@ -154,7 +154,11 @@ def train_audio_vqvae(model_name, load=False, epochs=500):
         
             # Actual training    
             optimiser.zero_grad()
-            recon_audio, quanitsed_loss = model(audio)    
+            recon_audio, quanitsed_loss = model(audio) 
+            
+            audio = audio[..., :430]
+            recon_audio = recon_audio[..., :430]
+               
             recon_loss = mse_loss(recon_audio, audio)
             loss = recon_loss + quanitsed_loss
             loss.backward()
